881ad046febfd74a75fd87ebbb513083a5f18df6
lemon-piepie <purplechilde@163.com>
2020-09-11 16:50:25 +0800

    problem one

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +17,11 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+
+  @JsonSerialize(using = TimeCampSerializer.class)
+  //@JsonDeserialize(using = TimeCampDeserializer.class)
   private Date time;
+
+  @JsonUnwrapped
   private UserVo user;
 }
---
+++ src/main/java/com/thoughtworks/capability/gtb/vo/TimeCampSerializer.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.capability.gtb.vo;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+public class TimeCampSerializer extends StdSerializer<Date> {
+
+    protected TimeCampSerializer() {
+        super(Date.class);
+    }
+
+    @Override
+    public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeNumber(date.getTime());
+    }
+}
